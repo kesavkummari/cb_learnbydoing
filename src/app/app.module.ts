88010b0f247a34dfba3b666ce150b7d57ad126c9
arun-jathari <jathariarunkumar@gmail.com>
2023-08-04 05:17:25 +0530

    first sections with buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { ButtonChooserComponent } from './_helpers/buttons/button.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonChooserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
