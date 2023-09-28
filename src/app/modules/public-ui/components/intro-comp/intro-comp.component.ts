109e93bb18178fe6ccce9e3d702b1faf1041b514
arun-jathari <jathariarunkumar@gmail.com>
2023-09-28 19:33:11 +0530

    intro-comp decentralized

--- src/app/modules/public-ui/components/intro-comp/intro-comp.component.ts
+++ src/app/modules/public-ui/components/intro-comp/intro-comp.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'intro-comp',
   templateUrl: './intro-comp.component.html',
+  styleUrls: ['./intro-comp.component.css']
 })
 export class IntroComponent {
 }
