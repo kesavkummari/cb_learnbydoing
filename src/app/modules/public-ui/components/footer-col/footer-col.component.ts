0f79fde3fd37221d1cb6df08f6ca50c315df1e19
arun-jathari <jathariarunkumar@gmail.com>
2023-09-28 20:38:46 +0530

    footer fix for mobile

--- src/app/modules/public-ui/components/footer-col/footer-col.component.ts
+++ src/app/modules/public-ui/components/footer-col/footer-col.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'footer-col',
   templateUrl: './footer-col.component.html',
+  styles: [`.col-md-3{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }`]
 })
 export class FooterComponent {
 }
