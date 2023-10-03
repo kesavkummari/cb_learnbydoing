4f79e7868ea504eef34c7ccf35237f8ff1107d88
balaganesh08 <70317990+balaganesh08@users.noreply.github.com>
2023-10-03 16:56:09 +0530

    courses table

--- src/app/modules/public-ui/components/trending-courses/course-table/course-table.component.ts
+++ src/app/modules/public-ui/components/trending-courses/course-table/course-table.component.ts
@@ -6,66 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-table.component.css']
 })
 export class CourseTableComponent {
-  osModules = [
-    { 
-      name: 'OS Module 1', 
-      content: ['Content for OS Module 1', '2', '3' ] } ,
-    { name: 'OS Module 2', content: ['Content for OS Module 2'] },
-    { name: 'OS Module 3', content: 'Content for OS Module 3' },
-    { name: 'OS Module 4', content: 'Content for OS Module 4' },
-    { name: 'OS Module 5', content: 'Content for OS Module 5' }
-  ];
-
-  awsModules = [
-    { name: 'AWS Services - Compute ',
-     content: [['Amazon EC2 -' , ' Images',
-     '  EBS',
-     ' Security Groups'],
-     
-      'Snapshots',
-      'Creating Golden Images',
-      'Amazon EC2 Auto Scaling',
-      'EC2 Image Builder',
-      'AWS Elastic Beanstalk'] },
-    { name: 'AWS Services - Storage', content: 'Content for AWS Module 2' },
-    { name: 'AWS Module 3', content: 'Content for AWS Module 3' },
-    { name: 'AWS Module 4', content: 'Content for AWS Module 4' },
-    { name: 'AWS Module 5', content: 'Content for AWS Module 5' },
-    { name: 'AWS Module 6', content: 'Content for AWS Module 6' }
-  ];
-
-  devOpsModules = [
-    { name: 'DevOps Module 1', content: 'Content for DevOps Module 1' },
-    { name: 'DevOps Module 2', content: 'Content for DevOps Module 2' },
-    { name: 'DevOps Module 3', content: 'Content for DevOps Module 3' },
-    { name: 'DevOps Module 4', content: 'Content for DevOps Module 4' },
-    { name: 'DevOps Module 5', content: 'Content for DevOps Module 5' }
-  ];
-
-  selectedModuleContent: string | null = null;
-  activeModules: any[] = [];
-
-  showModules(category: string) {
-    switch (category) {
-      case 'os':
-        this.activeModules = this.osModules;
-        break;
-      case 'aws':
-        this.activeModules = this.awsModules;
-        break;
-      case 'devops':
-        this.activeModules = this.devOpsModules;
-        break;
-      default:
-        this.activeModules = [];
-        break;
-    }
-    this.selectedModuleContent = null;
-  }
-
-  showModuleContent(moduleContent: string) {
-    this.selectedModuleContent = moduleContent;
-    
-  }
+    selectedTab: string = 'os'; // Default selected tab
+    selectedModule: string = 'M1'; // Default selected module
   
-}
+    selectTab(tabName: string) {
+      this.selectedTab = tabName;
+      this.selectedModule = 'M1'; // Reset selected module when tab changes
+    }
+  
+    selectModule(moduleName: string) {
+      this.selectedModule = moduleName;
+    }
+  }
