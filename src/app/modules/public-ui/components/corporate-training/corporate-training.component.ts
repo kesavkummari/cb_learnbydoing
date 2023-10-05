e1799ca23bf7da4b776e95d8558f52738206bcf3
arun-jathari <66483319+arun-jathari@users.noreply.github.com>
2023-10-05 08:27:58 +0000

    css fix

--- src/app/modules/public-ui/components/corporate-training/corporate-training.component.ts
+++ src/app/modules/public-ui/components/corporate-training/corporate-training.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'corporate-training',
   templateUrl: './corporate-training.component.html',
+  styleUrls:['./corporate-training.component.css']
 })
 export class CorporateTrainingComponent {
 }
