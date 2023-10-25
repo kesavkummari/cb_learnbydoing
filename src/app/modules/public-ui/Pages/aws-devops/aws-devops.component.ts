36ac64167d8235a9ea5062c3fec437c200ade737
balaganesh08 <70317990+balaganesh08@users.noreply.github.com>
2023-10-25 15:18:26 +0530

    aws page

--- src/app/modules/public-ui/Pages/aws-devops/aws-devops.component.ts
+++ src/app/modules/public-ui/Pages/aws-devops/aws-devops.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './aws-devops.component.html',
   styleUrls: ['./aws-devops.component.css']
 })
-export class AWSDevOpsComponent {}
+export class AWSDevOpsComponent {
+ 
+}
