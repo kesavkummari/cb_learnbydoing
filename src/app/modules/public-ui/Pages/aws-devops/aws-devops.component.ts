35ded3a5268d42d8dac2775dd522f563aa83058f
balaganesh08 <70317990+balaganesh08@users.noreply.github.com>
2023-10-25 20:17:00 +0530

    link for course content on AWS pages

--- src/app/modules/public-ui/Pages/aws-devops/aws-devops.component.ts
+++ src/app/modules/public-ui/Pages/aws-devops/aws-devops.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'aws-devops',
   templateUrl: './aws-devops.component.html',
   styleUrls: ['./aws-devops.component.css']
 })
-export class AWSDevOpsComponent {
- 
+export class AWSDevOpsComponent implements OnInit {
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {}
+
+  scrollToTrendingCourses() {
+    const trendingCourses = this.elementRef.nativeElement.querySelector('#trending-courses');
+
+    if (trendingCourses) {
+      trendingCourses.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
