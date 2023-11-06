71e265f86316f0bc337fb3931c115c1b9efb5fd1
balaganesh08 <70317990+balaganesh08@users.noreply.github.com>
2023-11-06 18:38:14 +0530

    trial course content mview

--- src/app/modules/public-ui/public-ui.modue.ts
+++ src/app/modules/public-ui/public-ui.modue.ts
@@ -25,9 +25,10 @@ import {MlOpsComponent} from './Pages/mlops/mlops.component'
 import {MlOpsCoursesComponent} from './components/mlops_course/mlops-courses.component'
 import {mlopsCourseTableComponent} from './components/mlops_course/mlops-table/mlopscourse-table.component'
 import { ImagePopupComponent } from './components/app-image-popup/image-popup.component'
+import {CourseContentMob} from './components/trending-courses/course-contentmob/course-contentmob'
 
 @NgModule({
-  declarations: [CloudIntroComponent,  IntroCardsComponent, ProgramsComponent, TrendingCoursesComponent, HybridLearningComponent, KeyFeaturesComponent, CorporateTrainingComponent, DigitalLyncComponent, QuestionsColComponent, LocationsComponent, CourseTableComponent, IntroPageComponent,CourseIntenseTableComponent,AWSDevOpsComponent, MlOpsComponent, MlOpsCoursesComponent , mlopsCourseTableComponent, ImagePopupComponent ],
+  declarations: [CloudIntroComponent,  IntroCardsComponent, ProgramsComponent, TrendingCoursesComponent, HybridLearningComponent, KeyFeaturesComponent, CorporateTrainingComponent, DigitalLyncComponent, QuestionsColComponent, LocationsComponent, CourseTableComponent, IntroPageComponent,CourseIntenseTableComponent,AWSDevOpsComponent, MlOpsComponent, MlOpsCoursesComponent , mlopsCourseTableComponent, ImagePopupComponent, CourseContentMob ],
   imports: [RouterModule, PublicUiRoutingModule, SharedModule, CommonModule],
   providers: [],
 })
