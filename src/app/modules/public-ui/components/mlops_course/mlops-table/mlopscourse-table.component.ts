abcd8a7a4f00ea7f3d64f2173184407efabe51e9
balaganesh08 <70317990+balaganesh08@users.noreply.github.com>
2023-12-01 22:21:16 +0530

    new Changes

--- src/app/modules/public-ui/components/mlops_course/mlops-table/mlopscourse-table.component.ts
+++ src/app/modules/public-ui/components/mlops_course/mlops-table/mlopscourse-table.component.ts
@@ -6,15 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./mlopscourse-table.component.css']
 })
 export class mlopsCourseTableComponent {
-  selectedTab: string = 'os'; // Default selected tab
-  selectedModule: string = 'M1'; // Default selected module
+ 
+
+  selectedTab: string = 'devops'; // Default selected tab
+  selectedModule: string | null = null; // Default selected module is null
+  showRightColumn: boolean = true; // Initialize to true to show the right column by default
+  isModuleSelected: boolean = false; // Initialize to false since module content should be hidden by default
+
+  constructor() {
+    // Optionally, you can set other default values or perform any initialization here.
+    this.isModuleSelected = false; // Ensure isModuleSelected is initially set to false
+  }
 
   selectTab(tabName: string) {
     this.selectedTab = tabName;
-    this.selectedModule = 'M1'; // Reset selected module when tab changes
+    this.selectedModule = null; // Reset selected module when tab changes
+    this.showRightColumn = false; // Hide the right column when changing tabs
+    this.isModuleSelected = false; // Reset the module selection flag
   }
 
   selectModule(moduleName: string) {
+    if (moduleName && moduleName !== 'M1') {
+      this.isModuleSelected = true; // Set to true when a module is selected
+      this.showRightColumn = true; // Show the right column when selecting a module
+    } else {
+      // Handle the case when 'M1' is clicked
+      this.isModuleSelected = false;
+      this.showRightColumn = false;
+    }
     this.selectedModule = moduleName;
   }
   }
