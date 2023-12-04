ac04659fcbb73d9ea441c028bedd9fd31b107d71
balaganesh08 <70317990+balaganesh08@users.noreply.github.com>
2023-12-04 20:07:43 +0530

    blog 4

---
+++ src/app/modules/public-ui/components/blogs-pages/blog4/route53.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog4',
+  templateUrl: './route53.html',
+
+})
+export class Blog4Component {
+}
--- src/app/modules/public-ui/public-ui-routing.module.ts
+++ src/app/modules/public-ui/public-ui-routing.module.ts
@@ -10,6 +10,7 @@ import  { BlogsComponent } from './components/blogs/blog.component'
 import { Blog1Component } from './components/blogs-pages/blog1/blog1';
 import { Blog2Component } from './components/blogs-pages/blog2/frugalarchitect';
 import { Blog3Component } from './components/blogs-pages/blog3/ec2';
+import { Blog4Component } from './components/blogs-pages/blog4/route53';
 
 const routes: Routes = [
   {path:'aws', component: AWSDevOpsComponent},
@@ -20,6 +21,7 @@ const routes: Routes = [
   { path: 'blog1' , component : Blog1Component },
   { path: 'frugalarchitect' , component : Blog2Component },
   { path: 'Amazon-EC2' , component : Blog3Component },
+  { path: 'Route-53' , component : Blog4Component },
   
   {
     path: '',
