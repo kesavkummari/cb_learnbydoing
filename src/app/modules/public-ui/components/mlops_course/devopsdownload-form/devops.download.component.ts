f7f231c9bc44078f5867045049daf6522b8dab2e
arun-jathari <jathariarunkumar@gmail.com>
2023-12-10 02:27:46 +0530

    add features to dev course content pdf

--- src/app/modules/public-ui/components/mlops_course/devopsdownload-form/devops.download.component.ts
+++ src/app/modules/public-ui/components/mlops_course/devopsdownload-form/devops.download.component.ts
@@ -2,6 +2,10 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // for template-driven forms
 // or
 import { ReactiveFormsModule } from '@angular/forms'; // for reactive forms
+import { DataService } from '../../../services/data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'devopsdownload-form',
@@ -11,24 +15,44 @@ import { ReactiveFormsModule } from '@angular/forms'; // for reactive forms
 export class DevopsDownloadComponent  { 
   popupform = false;
   name = 'Angular';
-  formData: { name: string, email: string, mobile: string } = { name: '', email: '', mobile: '' };
+  formData: { name: string, email: string, phone: string } = { name: '', email: '', phone: '' };
 
   // Reference to the hidden link element
   @ViewChild('pdfDownloadLink', { static: false })
   pdfDownloadLink!: ElementRef;
 
+  constructor
+  (
+    private elementRef: ElementRef,
+    private snackBar: MatSnackBar,
+    private http: HttpClient,
+    private dataService: DataService
+  ) {}
+
   // Function to handle the download and close
   handleDownloadAndClose() {
-    // Trigger the download by simulating a click on the hidden link
-    this.pdfDownloadLink.nativeElement.click();
+    // // Trigger the download by simulating a click on the hidden link
+    // this.pdfDownloadLink.nativeElement.click();
+    // Insert form data into API
+    this.dataService.submitForm(this.formData).subscribe((res: any) => {
+      // console.log(res);
+      this.http.get('https://8amcloudbinary.s3.amazonaws.com/AWS_DevOps.pdf', { responseType: 'blob' })
+      .subscribe((response: Blob) => {
+        saveAs(response, 'CB DevOps CourseCurriculum.pdf');
+        // Show success message
+        this.snackBar.open('Form submitted successfully!', 'Close', {
+          duration: 5000
+        });
+        // Close the form
+        this.popupform = false;
+        });
+    });
 
-    // Close the form
-    this.popupform = false;
   }
 
   // Function to handle form submission
   handleSubmitForm() {
-    if (this.formData.name && this.formData.email && this.formData.mobile) {
+    if (this.formData.name && this.formData.email && this.formData.phone) {
       this.handleDownloadAndClose(); // Download the PDF and close the form
     } else {
       alert('Please fill in all the form fields before submitting.');
