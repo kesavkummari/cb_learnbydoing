93165cc5e7731a8db0dfb48bc7589276e32f0f1c
balaganesh08 <70317990+balaganesh08@users.noreply.github.com>
2023-12-09 13:10:18 +0530

    Thank You Page and links

--- src/app/modules/public-ui/public-ui-routing.module.ts
+++ src/app/modules/public-ui/public-ui-routing.module.ts
@@ -6,13 +6,14 @@ import { IntroPageComponent } from './Pages/intro-page/intro-page.component';
 import { AWSDevOpsComponent } from './Pages/aws/aws.component';
 import { MlOpsComponent } from './Pages/mlops/mlops.component';
 import { ContactUsComponent } from './components/contactus/contact.component';
-import  { BlogsComponent } from './components/blogs/blog.component'
+import { BlogsComponent } from './components/blogs/blog.component'
 import { Blog1Component } from './components/blogs-pages/blog1/blog1';
 import { Blog2Component } from './components/blogs-pages/blog2/frugalarchitect';
 import { Blog3Component } from './components/blogs-pages/blog3/ec2';
 import { Blog4Component } from './components/blogs-pages/blog4/route53';
 import { Blog5Component } from './components/blogs-pages/blog5/dynamodb';
 import { Blog6Component } from './components/blogs-pages/blog6/s3';
+import { ThankyouComponent } from './components/thankyou/thankyou.component'
 
 
 const routes: Routes = [
@@ -27,6 +28,7 @@ const routes: Routes = [
   { path: 'Route-53' , component : Blog4Component },
   { path: 'AWS-DynamoDB' , component : Blog5Component },
   { path: 'Amazon-S3' , component : Blog6Component },
+  { path: 'thank-you' , component : ThankyouComponent },
   
   
   {
