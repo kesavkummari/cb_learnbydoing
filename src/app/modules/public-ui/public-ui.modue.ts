565370626a423597aca86ed1c1f8cade7d5f5941
balaganesh08 <bbg4102k@gmail.com>
2023-12-21 17:59:06 +0530

    Tools

--- src/app/modules/public-ui/public-ui.modue.ts
+++ src/app/modules/public-ui/public-ui.modue.ts
@@ -46,10 +46,11 @@ import { CertificateComponent } from './components/certificate/certificate.compo
 import { SubHeaderComponent } from './components/subhead/subheader.component';
 import { FeeStructureComponent } from './components/fee-structure/fee.structure.component';
 import { ToolsComponent } from './components/tools/tools.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 
 @NgModule({
-  declarations: [CloudIntroComponent,  IntroCardsComponent, ProgramsComponent, TrendingCoursesComponent, HybridLearningComponent, KeyFeaturesComponent, CorporateTrainingComponent, DigitalLyncComponent, QuestionsColComponent, LocationsComponent, CourseTableComponent, IntroPageComponent,CourseIntenseTableComponent,AWSDevOpsComponent, MlOpsComponent, MlOpsCoursesComponent , mlopsCourseTableComponent, ImagePopupComponent, CourseContentMob, RegisterFormComponent, DevopsDownloadComponent, DevOpsCourseContentMob, Blog1Component, FAQComponent, Blog2Component,ContactUsComponent,  CertificateComponent, SubHeaderComponent, FeeStructureComponent , ToolsComponent ],
+  declarations: [CloudIntroComponent,  IntroCardsComponent, ProgramsComponent, TrendingCoursesComponent, HybridLearningComponent, KeyFeaturesComponent, CorporateTrainingComponent, DigitalLyncComponent, QuestionsColComponent, LocationsComponent, CourseTableComponent, IntroPageComponent,CourseIntenseTableComponent,AWSDevOpsComponent, MlOpsComponent, MlOpsCoursesComponent , mlopsCourseTableComponent, ImagePopupComponent, CourseContentMob, RegisterFormComponent, DevopsDownloadComponent, DevOpsCourseContentMob, Blog1Component, FAQComponent, Blog2Component,ContactUsComponent,  CertificateComponent, SubHeaderComponent, FeeStructureComponent , ToolsComponent, TestimonialsComponent ],
   imports: [RouterModule, PublicUiRoutingModule, SharedModule, CommonModule, FormsModule, ReactiveFormsModule,MatFormFieldModule,MatInputModule, MatSelectModule,MatTooltipModule,MatIconModule,MatSnackBarModule,HttpClientModule],
   providers: [],
 })
