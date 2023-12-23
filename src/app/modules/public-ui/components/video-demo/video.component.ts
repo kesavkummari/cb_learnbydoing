cc51570836fc762c6c64a10453638682503eeefa
balaganesh08 <bbg4102k@gmail.com>
2023-12-23 14:37:32 +0530

    Yt Videos and Some content Changes

---
+++ src/app/modules/public-ui/components/video-demo/video.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'video-demo',
+  templateUrl: './video.component.html',
+  styleUrls:['./video.component.css']
+})
+export class VideoComponent {
+}
--- src/app/modules/public-ui/public-ui.modue.ts
+++ src/app/modules/public-ui/public-ui.modue.ts
@@ -48,10 +48,18 @@ import { FeeStructureComponent } from './components/fee-structure/fee.structure.
 import { ToolsComponent } from './components/tools/tools.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { BlogComponent } from './components/blog/blog';
+import { VideoComponent } from './components/video-demo/video.component';
 
 
 @NgModule({
-  declarations: [CloudIntroComponent,  IntroCardsComponent, ProgramsComponent, TrendingCoursesComponent, HybridLearningComponent, KeyFeaturesComponent, CorporateTrainingComponent, DigitalLyncComponent, QuestionsColComponent, LocationsComponent, CourseTableComponent, IntroPageComponent,CourseIntenseTableComponent,AWSDevOpsComponent, MlOpsComponent, MlOpsCoursesComponent , mlopsCourseTableComponent, ImagePopupComponent, CourseContentMob, RegisterFormComponent, DevopsDownloadComponent, DevOpsCourseContentMob, Blog1Component, FAQComponent, Blog2Component,ContactUsComponent,  CertificateComponent, SubHeaderComponent, FeeStructureComponent , ToolsComponent, TestimonialsComponent, BlogComponent ],
+  declarations: [CloudIntroComponent,  IntroCardsComponent, ProgramsComponent, TrendingCoursesComponent, HybridLearningComponent, KeyFeaturesComponent, CorporateTrainingComponent, DigitalLyncComponent, QuestionsColComponent, LocationsComponent, CourseTableComponent, 
+    IntroPageComponent,CourseIntenseTableComponent,
+    AWSDevOpsComponent, MlOpsComponent, MlOpsCoursesComponent , 
+    mlopsCourseTableComponent, ImagePopupComponent, CourseContentMob, 
+    RegisterFormComponent, DevopsDownloadComponent, DevOpsCourseContentMob, 
+    Blog1Component, FAQComponent, Blog2Component,ContactUsComponent, 
+     CertificateComponent, SubHeaderComponent, FeeStructureComponent , 
+     ToolsComponent, TestimonialsComponent, BlogComponent, VideoComponent ],
   imports: [RouterModule, PublicUiRoutingModule, SharedModule, CommonModule, FormsModule, ReactiveFormsModule,MatFormFieldModule,MatInputModule, MatSelectModule,MatTooltipModule,MatIconModule,MatSnackBarModule,HttpClientModule],
   providers: [],
 })
