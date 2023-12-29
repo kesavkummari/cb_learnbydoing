0262e91e9dca86aaa772d20447d83f82f7cab1ba
balaganesh08 <bbg4102k@gmail.com>
2023-12-29 10:35:51 +0530

    Certificate

--- src/app/modules/public-ui/components/certificate/certificate.component.ts
+++ src/app/modules/public-ui/components/certificate/certificate.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'certificate',
   templateUrl: './certificate.component.html',
+  styleUrls:['./certificate.component.css']
 
 })
 export class CertificateComponent { 
+  certificateType: string = 'course'; // Set the default certificate type
+
+  showCertificate(type: string): void {
+    this.certificateType = type;
+  }
 }
