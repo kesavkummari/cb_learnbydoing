513b496aa25f8640dd82e94ad5c25bc1a707889e
balaganesh08 <bbg4102k@gmail.com>
2024-01-11 14:29:53 +0530

    Kloud Binary LINK & Popup changed

--- src/app/modules/public-ui/Pages/intro-page/intro-page.component.ts
+++ src/app/modules/public-ui/Pages/intro-page/intro-page.component.ts
@@ -6,7 +6,19 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['intro-page.component.css']
 })
 export class IntroPageComponent { 
-  public isPopupOpen: boolean = true;
+  public isPopupOpen: boolean = false; // Start with popup closed initially
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    // You can adjust the scroll position at which you want to open the popup
+    const scrollPosition = window.scrollY;
+    const triggerScrollPosition = 200; // Adjust this value as needed
+
+    // Check if the scroll position is beyond the trigger point
+    if (scrollPosition > triggerScrollPosition) {
+      this.isPopupOpen = true;
+    } else {
+      this.isPopupOpen = false;
+    }
+  }
 }
-
-
