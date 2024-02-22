9c6131a0e4dffe133e9ca127a788be7338fe5569
kesav-at-420815905200 <b11.cloudbinary@gmail.com>
2024-02-22 16:26:27 +0530

    new batch updates

--- src/app/shared/components/cards/simple-card/simple-card.component.ts
+++ src/app/shared/components/cards/simple-card/simple-card.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class SimpleCardComponent {
   @Input() ImgSrc: string = "https://www.digital-lync.com/images/student1.png";
-  @Input() ImgTitle: string = "Learn From Best Cartoon- Cloud Binary Best Classroom & Online Training";
+  @Input() ImgTitle: string = "Learn From Cloud Binary Best Online Training";
   @Input() CardTitle: string = "Learn From Best";
   @Input() CardDesc: string = "We don’t settle or let you settle for nothing less than the BEST.";
 }
