85862bb9cb8f7ed0c5ab86a31f67550d0dc5ef78
balaganesh08 <bbg4102k@gmail.com>
2024-03-06 12:17:53 +0530

    first page

--- src/app/modules/public-ui/components/home-page-new/page1.ts
+++ src/app/modules/public-ui/components/home-page-new/page1.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'home-page-new',
@@ -10,22 +10,48 @@ import { Router } from '@angular/router';
 export class HomePageNewComponent {
   myForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.myForm = this.fb.group({
       name: ['', Validators.required],
       phone: ['', Validators.required],
-      option: ['option1', Validators.required]
+      option: ['', Validators.required] // Added Validators.required for the 'option' field
     });
   }
 
   onSubmit() {
-    const name = this.myForm!.get('name')!.value;
-const phone = this.myForm!.get('phone')!.value;
+    if (this.myForm.valid) {
+      const name = this.myForm.get('name')!.value;
+      const phone = this.myForm.get('phone')!.value;
+      const option = this.myForm.get('option')!.value;
 
+      const formData = {
+        name: name,
+        mobile: phone,
+        looking_for: option
+      };
 
-    const message = `Hi, I have visited your website and I am excited to know about AWS & DevOps Course Details. Could you please help me?\nName: ${name}\nPhone: ${phone}`;
-    const whatsappURL = `https://api.whatsapp.com/send/?phone=919100073006&text=${encodeURIComponent(message)}`;
+      this.http.post('https://irt3364303.execute-api.us-east-1.amazonaws.com/dev_portal/OneStepAway_Form', formData)
+        .subscribe(response => {
+          console.log('Form data submitted successfully', response);
 
-    window.location.href = whatsappURL;
+          const message = `Hi, I have visited your website and I am excited to know about AWS & DevOps Course Details. Could you please help me?\nName: ${name}\nPhone: ${phone}`;
+          const whatsappURL = `https://api.whatsapp.com/send/?phone=919100073006&text=${encodeURIComponent(message)}`;
+
+          window.location.href = whatsappURL;
+
+          this.myForm.reset();
+        }, error => {
+          console.error('Error submitting form data', error);
+        });
+    } else {
+      console.error('Form is not valid. Please check the fields.');
+      
+      Object.keys(this.myForm.controls).forEach(key => {
+        const control = this.myForm.get(key)!;
+        console.log(`Control '${key}':`);
+        console.log('  Status:', control.status);
+        console.log('  Errors:', control.errors);
+      });
+    }
   }
 }
