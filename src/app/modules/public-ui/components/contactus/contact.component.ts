7ed179bb102300a67500353d00b960ef80dbef53
balaganesh08 <bbg4102k@gmail.com>
2024-04-23 09:40:41 +0530

    Changes

--- src/app/modules/public-ui/components/contactus/contact.component.ts
+++ src/app/modules/public-ui/components/contactus/contact.component.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DataService } from '../../services/data.service';
-import { saveAs } from 'file-saver';
 import { Router } from '@angular/router'; // Import Router
 
 @Component({
@@ -13,36 +12,26 @@ import { Router } from '@angular/router'; // Import Router
 export class ContactUsComponent {
   formData: { first_name: string, last_name:string, email: string, phone: string, user_message:string} = { first_name: '',last_name:'', email: '', phone: '' ,user_message:''};
 
-  constructor
-  (
+  constructor(
     private snackBar: MatSnackBar,
     private http: HttpClient,
     private dataService: DataService,
     private router: Router // Inject Router
   ) {}
 
-  // Function to handle the download and close
-  handleDownloadAndClose() {
-    // Insert form data into API
-    this.dataService.submitForm(this.formData).subscribe((res: any) => {
-      this.http.get('https://8amcloudbinary.s3.amazonaws.com/AWS_DevOps_Regular_CloudBinary_20240125.pdf', { responseType: 'blob' })
-      .subscribe((response: Blob) => {
-        saveAs(response, 'CB DevOps CourseCurriculum.pdf');
+  // Function to handle form submission
+  handleSubmitForm() {
+    if (this.formData.first_name && this.formData.last_name && this.formData.email && this.formData.phone && this.formData.user_message) {
+      // Submit form data to API
+      this.dataService.submitForm(this.formData).subscribe(() => {
         // Show success message
         this.snackBar.open('Form submitted successfully!', 'Close', {
           duration: 5000
         });
-
+        
         // Navigate to the "/thank-you" page
         this.router.navigate(['/thank-you']);
       });
-    });
-  }
-
-  // Function to handle form submission
-  handleSubmitForm() {
-    if (this.formData.first_name && this.formData.last_name && this.formData.email && this.formData.phone && this.formData.user_message) {
-      this.handleDownloadAndClose(); // Download the PDF and close the form
     } else {
       alert('Please fill in all the form fields before submitting.');
     }
