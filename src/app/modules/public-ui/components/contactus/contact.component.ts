ad422c89edada56095cd6d701b000e7f7935bf6b
balaganesh08 <bbg4102k@gmail.com>
2024-05-21 13:52:52 +0530

    Contact us form

--- src/app/modules/public-ui/components/contactus/contact.component.ts
+++ src/app/modules/public-ui/components/contactus/contact.component.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DataService } from '../../services/data.service';
-import { Router } from '@angular/router'; // Import Router
 
 @Component({
   selector: 'contactus',
@@ -15,35 +14,28 @@ export class ContactUsComponent {
   constructor(
     private snackBar: MatSnackBar,
     private http: HttpClient,
-    private dataService: DataService,
-    private router: Router // Inject Router
+    private dataService: DataService
   ) { }
 
-  // Function to handle the form submission and close
-  handleFormSubmissionAndClose() {
+  // Function to handle the form submission, reset, and redirect to WhatsApp
+  handleFormSubmissionResetAndRedirect() {
     // Insert form data into API
     this.dataService.submitForm(this.formData).subscribe({
       next: () => {
-        // PDF download is commented out for now
-        // this.http.get('https://8amcloudbinary.s3.amazonaws.com/AWS_DevOps_Regular_CloudBinary_20240125.pdf', { responseType: 'blob' })
-        // .subscribe((response: Blob) => {
-        //   saveAs(response, 'CB DevOps CourseCurriculum.pdf');
-          
-          // Show success message
-          this.snackBar.open('Form submitted successfully!', 'Close', {
-            duration: 5000
-          });
+        // Show success message
+        this.snackBar.open('Form submitted successfully!', 'Close', {
+          duration: 5000
+        });
 
-          // Navigate to the "/thank-you" page
-          this.router.navigate(['/thank-you']);
-          
-          // Construct the WhatsApp message
-          const message = `Hi, I have visited your website and I am excited to know about AWS & DevOps Course Details. Could you please help me?\nName: ${this.formData.first_name} ${this.formData.last_name}\nPhone: ${this.formData.phone}`;
-          const whatsappURL = `https://api.whatsapp.com/send/?phone=919100073006&text=${encodeURIComponent(message)}`;
-          
-          // Redirect to WhatsApp
-          window.open(whatsappURL, '_blank');
-        // });
+        // Construct the WhatsApp message
+        const message = `Hi, I have visited your website and I am excited to know about AWS & DevOps Course Details. Could you please help me?\nName: ${this.formData.first_name} ${this.formData.last_name}\nPhone: ${this.formData.phone}`;
+        const whatsappURL = `https://api.whatsapp.com/send/?phone=919100073006&text=${encodeURIComponent(message)}`;
+
+        // Reset the form
+        this.resetForm();
+
+        // Redirect to WhatsApp
+        window.open(whatsappURL, '_blank');
       },
       error: (err) => {
         console.error('Error submitting the form', err);
@@ -62,10 +54,15 @@ export class ContactUsComponent {
         duration: 3000
       });
 
-      this.handleFormSubmissionAndClose(); // Submit the form and close
+      this.handleFormSubmissionResetAndRedirect(); // Submit the form, reset, and redirect to WhatsApp
     } else {
       alert('Please fill in all the form fields before submitting.');
     }
+  }
+
+  // Function to reset the form
+  resetForm() {
+    this.formData = { first_name: '', last_name: '', email: '', phone: '', user_message: '' };
   }
 
   validateEmail(email: string) {
