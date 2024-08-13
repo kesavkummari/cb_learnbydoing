a31dbf9663824f2046390d531321add3487154e0
ck <ck@ck.local>
2024-08-13 10:17:12 +0530

    upcoming batch data updated

--- src/app/modules/public-ui/components/course-content-component/aws-devops-course/regularawsmobilecc/regularaws.ts
+++ src/app/modules/public-ui/components/course-content-component/aws-devops-course/regularawsmobilecc/regularaws.ts
@@ -12,7 +12,7 @@ export class RegularAWSCourseContent {
 
   courses = [
     {
-      title: 'AWS DevSecOps Job Ready Program',
+      title: 'Mastering Cloud Engineering & Operations Program',
       modules: [
         // Course 2 modules and submodules
         // Module 1
