fdc98c0001c6768e5664df746da06aa3718924cb
ck <ck@ck.local>
2024-09-04 18:52:14 +0530

    finops

--- src/app/modules/public-ui/components/course-content-component/aws-devops-course/devopscourse-contentmob/devopscourse-contentmob.ts
+++ src/app/modules/public-ui/components/course-content-component/aws-devops-course/devopscourse-contentmob/devopscourse-contentmob.ts
@@ -11,7 +11,7 @@ export class DevOpsCourseContentMob {
 
   courses = [
     {
-      title: 'AWS MLOps',
+      title: 'FinOps',
       modules: [
         // Course 2 modules and submodules
         // Module 1
