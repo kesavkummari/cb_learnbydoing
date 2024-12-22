b31920b6060543e907515ed6e91ba8ebf4e048db
ck <ck@ck.local>
2024-12-22 16:46:22 +0530

    cloud and devops

--- src/app/modules/public-ui/components/course-content-component/aws-devops-course/devopscourse-contentmob/devopscourse-contentmob.ts
+++ src/app/modules/public-ui/components/course-content-component/aws-devops-course/devopscourse-contentmob/devopscourse-contentmob.ts
@@ -11,7 +11,7 @@ export class DevOpsCourseContentMob {
 
   courses = [
     {
-      title: 'Cloud FinOps',
+      title: 'AWS',
       modules: [
         // Course 2 modules and submodules
         // Module 1
