1ac9fda052eb7b29588b44f1fe7447204837fa8b
arun-jathari <jathariarunkumar@gmail.com>
2024-12-30 09:19:45 +0530

    blog template fix for python coding

--- src/app/modules/public-ui/components/blogs-component/blogs-pages/all-blogs/da-python-basics/da-python-basics.component.ts
+++ src/app/modules/public-ui/components/blogs-component/blogs-pages/all-blogs/da-python-basics/da-python-basics.component.ts
@@ -6,14 +6,29 @@ import { Component } from '@angular/core';
   styleUrls:['../blog-template.component.css']
 })
 export class DaPythonBasicsComponent {
-  copyCode(event: any) { 
-    const code = event.target.previousElementSibling.innerText; 
-    navigator.clipboard.writeText(code).then(() => {
+  // copyCode(event: any) { 
+  //   const code = event.target.previousElementSibling.innerText; 
+  //   navigator.clipboard.writeText(code).then(() => {
+  //     const button = event.target; 
+  //     button.innerText = 'Copied!'; 
+  //     setTimeout(() => { 
+  //       button.innerText = 'Copy'; 
+  //     }, 2000); 
+  //   }); 
+  // }
+
+  async copyCode(event: any): Promise<void> { 
+    const codeContent = document.getElementById('code-content')!.innerText; 
+    try { 
+      await navigator.clipboard.writeText(codeContent); 
       const button = event.target; 
       button.innerText = 'Copied!'; 
       setTimeout(() => { 
-        button.innerText = 'Copy'; 
-      }, 2000); 
-    }); 
+              button.innerText = 'Copy'; 
+            }, 2000); 
+    } catch (err) { 
+      console.error('Failed to copy: ', err); 
+      alert('Failed to copy the code.'); 
+    } 
   }
 }
