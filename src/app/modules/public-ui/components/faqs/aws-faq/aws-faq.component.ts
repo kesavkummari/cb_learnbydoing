76c0ed16af38a252d1082038f965ddd6b30dd23f
arun-jathari <jathariarunkumar@gmail.com>
2025-02-14 11:52:44 +0530

    refactor minor changes

--- src/app/modules/public-ui/components/faqs/aws-faq/aws-faq.component.ts
+++ src/app/modules/public-ui/components/faqs/aws-faq/aws-faq.component.ts
@@ -6,5 +6,16 @@ import { Component, ElementRef, AfterViewInit, Renderer2, Input } from '@angular
   styleUrls: ['../faq.component.css']
 })
 export class AwsFaqComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  ngAfterViewInit() {
+    const faqQuestions = this.el.nativeElement.querySelectorAll('.faq-question');
+
+    faqQuestions.forEach((question: { parentNode: any; }) => {
+      this.renderer.listen(question, 'click', () => {
+        const faqItem = question.parentNode;
+        faqItem.classList.toggle('active');
+      });
+    });
+  }
 }
