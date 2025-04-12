e166bac5408a6380106e7aee09a10475f5bf0583
kesavkummari <kesav.kummari@gmail.com>
2025-04-12 12:58:18 +0530

    upcoming dates

--- src/app/modules/public-ui/Pages/page-not-found/page-not-found.component.ts
+++ src/app/modules/public-ui/Pages/page-not-found/page-not-found.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  template: `<h2>Page Not Found</h2>`,
+  template: `<h2>Coming Soon! - FinOps Certified Practitioner </h2>`,
   styles: [`h2 { color: red; margin: 200px}`]
 })
 export class PageNotFoundComponent {}
